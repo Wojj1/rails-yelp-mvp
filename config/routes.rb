a49f88dbf6f3d5833025e032a195b88361b0296d
wojciech janic <w.janic94@gmail.com>
2023-05-04 19:42:04 +0100

    scaffold review

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :reviews
   resources :restaurants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-resources :restaurants
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_183001) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_184057) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "phone_number"
     t.string "category"
     t.string "review"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
